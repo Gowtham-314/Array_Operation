57d5449fa69ac9c87a851ddd69737db0c848e9e0
Gowtham-314 <gowdagowtham7930@gmail.com>
2025-12-24 00:52:46 +0530

    Take a input from the user as a array

--- Traversal.py
+++ Traversal.py
@@ -1,1 +1,8 @@
+print("Array Traversal in Python Using an NumPy Library")
 from numpy import *
+try:
+    myarray=array(input("Enter elements separated by spaces: ").split(),int)
+except:
+    print("Invalid input. Please enter integers separated by spaces.")
+    
+print("Array elements are:",myarray)

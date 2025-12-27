65a1c64a669aab081be96504b459ae7ca0514b2d
Gowtham-314 <gowdagowtham7930@gmail.com>
2025-12-27 21:58:37 +0530

    Insertion Sort Algorithm Array

---
+++ InsertionSort.py
@@ -0,0 +1,35 @@
+"""---Array Sort in Python Using an NumPy Library---"""
+
+#Program to Sorting a elements in an array using NumPy.
+
+#    Insertion Sort Algorithm
+
+from numpy import *
+
+class InsertionSort:
+    
+    def array_input(self):
+        try:
+            self.my_array=array(input("Enter elements separated by spaces: ").split(),int)
+            # Binary search assumes sorted order; keep a sorted copy for searching.
+            self.my_array=sort(self.my_array)
+        except:
+            print("Invalid input. Please enter integers separated by spaces.")
+            exit()
+    
+    def array_sort(self):
+        
+        for i in range(1,len(self.my_array)):
+            j=i
+            while j>=1:
+                if self.my_array[j]<self.my_array[j-1]:
+                    self.my_array[j-1],self.my_array[j]==self.my_array[j],self.my_array[j-1]
+            j-=1
+
+
+arr_sort=InsertionSort()
+arr_sort.array_input()
+print("Array Before the Sort : ",arr_sort.my_array)
+arr_sort.array_sort()
+print("\nArray After the Sort : ",arr_sort.my_array)
+exit()
